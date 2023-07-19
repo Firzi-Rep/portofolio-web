d1018d9791fb8d38ed7f9443c750619f12324c80
Firzi <firziawan20@gmail.com>
2023-07-20 00:59:38 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,216 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import GithubIcon from '../../public/icons/github'
+import InstagramIcon from '../../public/icons/instagram'
+import FacebookIcon from '../../public/icons/facebook'
+
+const navMenu = [
+  { href: '/home', title: 'home/' },
+  { href: '/project', title: 'project/' },
+  { href: '/contact', title: 'contact/' }
+]
+
+const icon = [
+  {
+    href: 'https://github.com/Firzi-Rep',
+    icon: GithubIcon
+  },
+  {
+    href: 'https://www.instagram.com/ahimsyafirzi/',
+    icon: InstagramIcon
+  },
+  {
+    href: 'https://id-id.facebook.com/ahimsyafirzi',
+    icon: FacebookIcon
+  }
+]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="px-4 py-12 font-rajdhani">
+      <header className="sticky w-full font-rajdhani">
+        <div className="py-4 max-w-5xl mx-auto flex flex-row justify-between items-center">
+          <div className="flex items-center space-x-7">
+            <Link href="/" legacyBehavior>
+              <a className="flex py-2 items-center space-x-2">
+                <img
+                  className="w-24 h-24 object-cover rounded-full"
+                  src="https://www.w3schools.com/w3images/avatar2.png"
+                />
+                <span className="font-bold text-xl">Portofolio</span>
+              </a>
+            </Link>
+            <nav className="hidden md:flex text-lg">
+              <ul className="flex space-x-3 underline">
+                {navMenu.map((navMenu, index) => (
+                  <li key={index}>
+                    <Link href={navMenu.href} legacyBehavior>
+                      <a className="px-3 py-2">{navMenu.title}</a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+          <div className="flex space-x-4">
+            {icon.map((icon) => (
+              <Link href={icon.href} legacyBehavior key={icon.href}>
+                <a>
+                  <icon.icon />
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="max-w-5xl mx-auto">
+          <div className="md:grid-cols-2 gap-4">
+            <div className="bg-white border border-gray-200 rounded-lg shadow dark:border-gray-500">
+              <div className="flex flex-col md:flex-row">
+                <img
+                  className="object-cover w-full md:w-60 rounded-t-lg h-96 md:h-auto md:rounded-none md:rounded-l-lg"
+                  src="https://www.w3schools.com/w3images/avatar2.png"
+                  alt=""
+                />
+                <div className="flex flex-col items-center justify-center p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center md:text-left">
+                    <span className="block text-center md:inline md:text-left">
+                      Ahimsya Firziawan
+                    </span>
+                  </h5>
+                  <p className="font-semibold text-center md:text-left">
+                    Ability in Quick and Effective Troubleshooting in a Back-End
+                    Environment, to identify and address complex technical
+                    challenges with efficient and innovative solutions.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div>
+          <div className="max-w-5xl mx-auto py-5">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <div className="grid grid-rows-1 grid-flow-col gap-4 text-center">
+              <div className="">
+                <section className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row dark:border-gray-500">
+                  <div className="flex flex-col justify-between p-4 leading-normal">
+                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 text-center md:text-left">
+                      <span className="block text-center underline md:inline md:text-left">
+                        PROJECT
+                      </span>
+                    </h5>
+                    <div className="font-rajdhani">
+                      <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+                        <li className="pb-3 pt-3 sm:pb-4">
+                            <div className="flex items-center space-x-72">
+                              <div className="flex-shrink-0">
+                                  <img className="w-48 h-48" src="https://www.w3schools.com/w3images/avatar2.png" alt="Neil image"/>
+                              </div>
+                              <div className="inline-flex-1 text-3xl">
+                                  <p className="font-semibold text-gray-900 truncate dark:text-dark">
+                                    Bookshelf-API
+                                  </p>
+                                  <p className="text-xl text-gray-500 truncate dark:text-gray-800">
+                                    www.project-1.com
+                                  </p>
+                              </div>
+                            </div>
+                        </li>
+                        <li className="pb-3 pt-3 sm:pb-4">
+                            <div className="flex items-center space-x-72">
+                              <div className="flex-shrink-0">
+                                  <img className="w-48 h-48" src="https://www.w3schools.com/w3images/avatar2.png" alt="Neil image"/>
+                              </div>
+                              <div className="inline-flex-1 text-3xl">
+                                  <p className="font-semibold text-gray-900 truncate dark:text-dark">
+                                    POS-API
+                                  </p>
+                                  <p className="text-xl text-gray-500 truncate dark:text-gray-800">
+                                    www.project-POS.com
+                                  </p>
+                              </div>
+                            </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <div className="gap-4 text-center py-5">
+              <div className="items-center">
+                <section className="items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row dark:border-gray-500">
+                  <div className="p-4">
+                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 text-center md:text-left">
+                      <span className="block text-center underline md:inline md:text-left">
+                        CONTACT
+                      </span>
+                    </h5>
+                    <div>
+                      <div className="">
+                        <form className="">
+                          <div className="mb-4">
+                            <label
+                              className="block text-gray-700 text-sm font-bold mb-2"
+                              htmlFor="name"
+                            >
+                              Name
+                            </label>
+                            <input
+                              className="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-600"
+                              id="name"
+                              type="text"
+                              placeholder="Enter your name"
+                            />
+                          </div>
+                          <div className="mb-4">
+                            <label
+                              className="block text-gray-700 text-sm font-bold mb-2"
+                              htmlFor="email"
+                            >
+                              Email
+                            </label>
+                            <input
+                              className="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-600"
+                              id="email"
+                              type="email"
+                              placeholder="Enter your email"
+                            />
+                          </div>
+                          <div className="mb-4">
+                            <label
+                              className="block text-gray-700 text-sm font-bold mb-2"
+                              htmlFor="message"
+                            >
+                              Message
+                            </label>
+                            <textarea
+                              className="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-600"
+                              id="message"
+                              placeholder="Enter your message"
+                            ></textarea>
+                          </div>
+                          <div className="flex justify-center">
+                            <button
+                              className="bg-gray-800 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                              type="button"
+                            >
+                              Send Message
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </div>
     </main>
   )
 }
