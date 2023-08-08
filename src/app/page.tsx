2115c4154dfb3620af936e7f624df2624cf840f1
Firzi <firziawan20@gmail.com>
2023-08-08 09:04:41 +0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,11 +25,16 @@ const icon = [
   },
 ];
 
+const gitLink = 'https://github.com/Firzi-Rep';
+const fblink = 'https://id-id.facebook.com/ahimsyafirzi';
+const iglinl = 'https://www.instagram.com/ahimsyafirzi/';
+const ldlink = 'https://www.linkedin.com/in/ahimsya-firziawan-a79761243/';
+
 export default function Home() {
   return (
-    <main className="px-4  font-rajdhani">
+    <main className="px-4 font-rajdhani">
       <header className="sticky w-full font-rajdhani">
-        <div className="py-4 max-w-5xl mx-auto flex flex-row justify-between items-center">
+        <div className="py-4 max-w-7xl mx-auto flex flex-row justify-between items-center">
           <div className="flex items-center space-x-7">
             <Link href="/" legacyBehavior>
               <a className="flex py-2 items-center space-x-2">
@@ -64,7 +69,7 @@ export default function Home() {
         </div>
       </header>
 
-      <div className="max-w-5xl py-6 mx-auto">
+      <div className="max-w-7xl py-6 mx-auto">
         <div className="md:grid-cols-2 gap-4">
           <div className="bg-white border border-gray-200 rounded-lg shadow dark:border-gray-500">
             <div className="flex flex-col md:flex-row">
@@ -91,9 +96,9 @@ export default function Home() {
       </div>
 
       <div>
-        <section className="max-w-5xl py-6 mx-auto bg-white border border-gray-200 dark:border-gray-500 overflow-hidden">
+        <section className="max-w-7xl py-6 mx-auto bg-white border border-gray-200 dark:border-gray-500 overflow-hidden">
           <div className="container mx-auto px-4">
-            <h2 className="mb-20 font-heading font-semibold text-center text-6xl sm:text-4xl text-black">
+            <h2 className="mb-8 font-heading font-semibold underline text-center text-4xl sm:text-4xl text-black">
               Project
             </h2>
             <div className="flex flex-wrap -m-3">
@@ -137,78 +142,118 @@ export default function Home() {
                   <div className="absolute bottom-0 left-0 w-full p-2.5"></div>
                 </div>
               </div>
+              <div className="w-full md:w-1/2 xl:w-1/4 p-3">
+                <div className="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
+                  <img
+                    className="mx-auto w-full"
+                    src="https://www.w3schools.com/w3images/avatar2.png"
+                    alt=""
+                  />
+                  <div className="absolute bottom-0 left-0 w-full p-2.5"></div>
+                </div>
+              </div>
+              <div className="w-full md:w-1/2 xl:w-1/4 p-3">
+                <div className="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
+                  <img
+                    className="mx-auto w-full"
+                    src="https://www.w3schools.com/w3images/avatar2.png"
+                    alt=""
+                  />
+                  <div className="absolute bottom-0 left-0 w-full p-2.5"></div>
+                </div>
+              </div>
+              <div className="w-full md:w-1/2 xl:w-1/4 p-3">
+                <div className="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
+                  <img
+                    className="mx-auto w-full"
+                    src="https://www.w3schools.com/w3images/avatar2.png"
+                    alt=""
+                  />
+                  <div className="absolute bottom-0 left-0 w-full p-2.5"></div>
+                </div>
+              </div>
+              <div className="w-full md:w-1/2 xl:w-1/4 p-3">
+                <div className="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
+                  <img
+                    className="mx-auto w-full"
+                    src="https://www.w3schools.com/w3images/avatar2.png"
+                    alt=""
+                  />
+                  <div className="absolute bottom-0 left-0 w-full p-2.5"></div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
       </div>
 
-      <div className="max-w-5xl py-6 mx-auto text-center">
-        <div className="items-center">
-          <section className="items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row dark:border-gray-500">
-            <div className="p-4">
-              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 text-center md:text-left">
-                <span className="block text-center underline md:inline md:text-left">
-                  CONTACT
-                </span>
-              </h5>
-              <div>
-                <div className="">
-                  <form className="">
-                    <div className="mb-4">
-                      <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
-                        htmlFor="name"
-                      >
-                        Name
-                      </label>
-                      <input
-                        className="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-600"
-                        id="name"
-                        type="text"
-                        placeholder="Enter your name"
-                      />
-                    </div>
-                    <div className="mb-4">
-                      <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
-                        htmlFor="email"
-                      >
-                        Email
-                      </label>
-                      <input
-                        className="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-600"
-                        id="email"
-                        type="email"
-                        placeholder="Enter your email"
-                      />
-                    </div>
-                    <div className="mb-4">
-                      <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
-                        htmlFor="message"
-                      >
-                        Message
-                      </label>
-                      <textarea
-                        className="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-600"
-                        id="message"
-                        placeholder="Enter your message"
-                      ></textarea>
-                    </div>
-                    <div className="flex justify-center">
-                      <button
-                        className="bg-gray-800 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="button"
-                      >
-                        Send Message
-                      </button>
-                    </div>
-                  </form>
+      <div>
+        <section className="max-w-7xl py-6 mx-auto bg-white  overflow-hidden">
+          <div className="relative z-10 container mx-auto px-4">
+            <div className="flex flex-wrap -m-6">
+              <div className="w-full md:w-1/2 p-6">
+                <div className="md:max-w-lg">
+                  <h2 className="mb-7 underline font-heading font-semibold text-4xl sm:text-3xl xl:text-4xl text-black">
+                    Contact Me
+                  </h2>
+                </div>
+              </div>
+              <div className="w-full md:w-1/2 p-6">
+                <div className="md:max-w-md ml-auto">
+                  <p className="mb-4 max-w-max underline text-black bg-clip-text bg-gradient-cyan2 font-heading text-lg font-semibold">
+                    Send me a mail
+                  </p>
+                  <p className="mb-16 text-black text-2xl">
+                    firziawan20@gmail.com
+                  </p>
+                  <p className="mb-4 max-w-max underline text-black  bg-clip-text bg-gradient-cyan2 font-heading text-lg font-semibold">
+                    Follow Me
+                  </p>
+                  <ul>
+                    <li className="mb-6 text-black hover:text-gray-600 text-2xl">
+                      <a href={fblink}>Facebook</a>
+                    </li>
+                    <li className="mb-6 text-black hover:text-gray-600 text-2xl">
+                      <a href={ldlink}>Linkedin</a>
+                    </li>
+                    <li className="mb-6 text-black hover:text-gray-600 text-2xl">
+                      <a href={iglinl}>Instagram</a>
+                    </li>
+                    <li className="text-black hover:text-gray-600 text-2xl">
+                      <a href={gitLink}>Github</a>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
+      </div>
+
+      <div>
+        <footer className="bg-white rounded-lg shadow max-w-7xl py-6 mx-auto dark:bg-white">
+          <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span className="text-sm text-black sm:text-center dark:text-black">
+              © 2023{' '}
+              <a
+                href="https://github.com/Firzi-Rep"
+                className="hover:underline"
+              >
+                Ahimsya Firziawan™
+              </a>
+              . All Rights Reserved.
+            </span>
+            <ul className="flex space-x-6 underline">
+              {icon.map((icon) => (
+                <Link href={icon.href} legacyBehavior key={icon.href}>
+                  <a>
+                    <icon.icon />
+                  </a>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </footer>
       </div>
     </main>
   );
